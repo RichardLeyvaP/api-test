0d6674d2f95c7be04991ffdf7cd2d01377931d21
RichardLeyvaP <richardleyvap1991@gmail.com>
2023-07-09 17:25:22 -0300

    RealizandoShow,Update,Delete

--- api-test/api-test/app/Http/Controllers/UserController.php
+++ api-test/api-test/app/Http/Controllers/UserController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUpdateUserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use GuzzleHttp\Psr7\Message;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -15,11 +18,42 @@ class UserController extends Controller
         return UserResource::collection($users);
     }
 
-    public function store(Request $request)
+    public function store(StoreUpdateUserRequest $request)
     {
-        $data = $request->all();
+        $data = $request->validated();
         $data['password'] = bcrypt($request->password);
         $user = User::create($data); 
         return new UserResource($user);
+    }
+
+    public function show(string $id)
+    {
+        //    $user = User::find($id);
+        // $user = User::where('id','=',$id)->get();
+        // $user = User::where('id','=',$id)->first();
+        // if(!$user)
+        // {
+        //     return response()->json(['message'=>'Usuario no encontrado'],404);
+        // }
+
+        $user = User::findOrFail($id);
+        return new UserResource($user);
+    }
+
+    public function update(StoreUpdateUserRequest $request,string $id)
+    {
+        $user = User::findOrFail($id);
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);        
+        $user->update($data);
+        return new UserResource($user);
+    }
+
+    public function delete(string $id)
+    {
+        $user = User::findOrFail($id);
+        $name = $user->name;
+        $user->delete();
+        return response()->json(['eliminado' => $name]);
     }
 }
